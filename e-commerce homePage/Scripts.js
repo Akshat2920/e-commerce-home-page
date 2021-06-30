b782bc2af713dee7845734903caee3a9dce5b542
Akshat2920 <81002264+Akshat2920@users.noreply.github.com>
2021-06-30 10:31:20 +0530

    Add files via upload

---
+++ e-commerce homePage/Scripts.js
@@ -0,0 +1,11 @@
+function openNav() {                                                     //Opening the sidebar
+    document.getElementById("mySidebar").style.width = "250px";
+  }
+  
+  function closeNav() {                                                  //Closing the sidebar
+    document.getElementById("mySidebar").style.width = "0";
+  }
+
+  function send(){
+      alert('Thank you for your feedback! Will get back to you within 24hrs')
+  }
